2a51ff0267b686490473833bfc84d8531b6fbabe
qadir <aqadir1989@gmail.com>
2026-06-04 10:15:39 +0530

    send-mesage-child-parent-viceversa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,40 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Child], //importing the child component
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-tytorial');
-  name = 'Angular';
-  isAdmin = true;
+  // protected readonly title = signal('angular-tytorial');
+  // name = 'Angular';
+  // isAdmin = true;
 
 
-  item = [10,20,15,35,67];
-  newItems = [10,1,21,5,8];
-  count = signal(10);
+  // item = [10,20,15,35,67];
+  // newItems = [10,1,21,5,8];
+  // count = signal(10);
 
-  increament() {
-    this.count.update((c) => c + 1);
-  }
+  // increament() {
+  //   this.count.update((c) => c + 1);
+  // }
 
-  decreament() {
-    this.count.update((c) => c - 1);
-  } 
+  // decreament() {
+  //   this.count.update((c) => c - 1);
+  // } 
 
-  reset() {
-    this.count.set(0);
-  }
+  // reset() {
+  //   this.count.set(0);
+  // }
+
+
+
+  //how to transfer data from parent to child component using @Input decorator.
+
+
+  userName : string = "Angular";  //strp1 : create a variable in parent component and assign some value to it.
 
 }
--- src/app/child/child.ts
+++ src/app/child/child.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input , signal} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './child.html',
   styleUrl: './child.css',
 })
-export class Child {}
+export class Child {
+  childUserName = input<string>(); //step2 : create a variable in child component and decorate it with @Input decorator and specify the type of data it will receive from parent component.
+
+  //update child to parent using signal
+
+  message = signal(''); //step1 : create a signal variable in child component to hold the data that will be sent to parent component.
+
+
+  updateMessage() {
+    this.message.set('Hello from child component!'); //step2 : update the signal variable with the data that you want to send to parent component.
+  }
+}
